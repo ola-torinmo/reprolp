a480709a8072c888957d640ed5caab27500c04e4
torinmo adebayo <torinmo.work@gmail.com>
2023-08-31 06:25:18 +0100

    deployment

--- src/component/BlogComp.jsx
+++ src/component/BlogComp.jsx
@@ -1,7 +1,7 @@
 import './Style.css';
 import React from 'react';
 import { Col,Container, Row } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 // import { useState } from 'react';
 import Tab from 'react-bootstrap/Tab';
